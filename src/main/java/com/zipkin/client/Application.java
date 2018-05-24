fa822d76361cb4e7292aea24c9081ffed993ff88
Virendra Singh <virendra.bisht2006@gmail.com>
2018-05-24 09:30:22 +0530

    Added protected constructor to avoid sonar error

--- src/main/java/com/zipkin/client/Application.java
+++ src/main/java/com/zipkin/client/Application.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    protected Application(){
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
