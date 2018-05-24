1cfed0de59a386cb92b344568e3b917946cf4059
Virendra Singh <virendra.bisht2006@gmail.com>
2018-05-24 09:28:29 +0530

    first commit, added main application and pom file

---
+++ src/main/java/com/zipkin/client/Application.java
@@ -0,0 +1,15 @@
+package com.zipkin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by esha on 24/05/18.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
